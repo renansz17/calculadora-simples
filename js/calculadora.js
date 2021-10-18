ca98da03ee2dd1b84843c021c5db0b7295f29339
renansz17 <renanszeremeta2@gmail.com>
2021-10-18 20:56:46 -0300

    a

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,4 +1,4 @@
-class Calculadora{
+export class Calculadora{
     constructor(textoOperacaoAnterior, textoOperacaoAtual){
         this.textoOperacaoAnterior = textoOperacaoAnterior
         this.textoOperacaoAtual = textoOperacaoAtual
@@ -87,41 +87,3 @@ class Calculadora{
     }
 
 }
-
-const btnsNumeros = document.querySelectorAll('[data-numero]')
-const btnsOperacao = document.querySelectorAll('[data-operacao]')
-const btnAllClear = document.querySelector('[data-allClear]')
-const btnDeleta = document.querySelector('[data-deleta]')
-const btnIgual = document.querySelector('[data-igual]')
-const textoOperacaoAnterior = document.querySelector('[data-operacaoAnterior]')
-const textoOperacaoAtual = document.querySelector('[data-operacaoAtual]')
-
-
-const calculadora = new Calculadora(textoOperacaoAnterior, textoOperacaoAtual)
-
-btnsNumeros.forEach(botao =>{
-    botao.addEventListener('click', ()=>{
-    calculadora.appendNumero(botao.innerText)
-    calculadora.atualizaDisplay()
-    })
-})
-
-btnsOperacao.forEach(botao =>{
-    botao.addEventListener('click', ()=>{
-        calculadora.escolhaOperacao(botao.innerText)
-        calculadora.atualizaDisplay()
-    })
-})
-
-btnIgual.addEventListener('click', botao => {
-    calculadora.compute()
-    calculadora.atualizaDisplay()
-})
-btnAllClear.addEventListener('click',botao =>{
-    calculadora.limpa()
-    calculadora.atualizaDisplay()
-})
-btnDeleta.addEventListener('click',botao =>{
-    calculadora.deleta()
-    calculadora.atualizaDisplay()
-})
---
+++ js/script.js
@@ -0,0 +1,39 @@
+import { Calculadora } from "./calculadora";
+
+const btnsNumeros = document.querySelectorAll('[data-numero]')
+const btnsOperacao = document.querySelectorAll('[data-operacao]')
+const btnAllClear = document.querySelector('[data-allClear]')
+const btnDeleta = document.querySelector('[data-deleta]')
+const btnIgual = document.querySelector('[data-igual]')
+const textoOperacaoAnterior = document.querySelector('[data-operacaoAnterior]')
+const textoOperacaoAtual = document.querySelector('[data-operacaoAtual]')
+
+
+const calculadora = new Calculadora(textoOperacaoAnterior, textoOperacaoAtual)
+
+btnsNumeros.forEach(botao =>{
+    botao.addEventListener('click', ()=>{
+    calculadora.appendNumero(botao.innerText)
+    calculadora.atualizaDisplay()
+    })
+})
+
+btnsOperacao.forEach(botao =>{
+    botao.addEventListener('click', ()=>{
+        calculadora.escolhaOperacao(botao.innerText)
+        calculadora.atualizaDisplay()
+    })
+})
+
+btnIgual.addEventListener('click', botao => {
+    calculadora.compute()
+    calculadora.atualizaDisplay()
+})
+btnAllClear.addEventListener('click',botao =>{
+    calculadora.limpa()
+    calculadora.atualizaDisplay()
+})
+btnDeleta.addEventListener('click',botao =>{
+    calculadora.deleta()
+    calculadora.atualizaDisplay()
+})
